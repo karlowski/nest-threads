31373523da26afc64b9856b47a74ca360c656d19
Stan Karlov <karlovvski@gmail.com>
2023-09-25 18:12:30 +0300

    added: time service for activity and creation record

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -5,9 +5,13 @@ import { PostsService } from './posts.service';
 import { PostsController } from './posts.controller';
 import { UserPost } from './post.entity';
 import { UserComment } from 'src/comments/comment.entity';
+import { SharedModule } from 'src/shared/shared.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserPost, UserComment])],
+  imports: [
+    TypeOrmModule.forFeature([UserPost, UserComment]),
+    SharedModule
+  ],
   providers: [PostsService],
   controllers: [PostsController]
 })
