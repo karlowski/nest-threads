d5df82e4f016a028cd2573c7c23664f60e42c092
Stanislav Karlov <karlovvski@gmail.com>
2024-01-14 16:37:57 +0200

    Added: separated data source for TypeORM & some migrations to play with

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,17 +7,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './api/v1/users/users.module';
 import { AuthModule } from './api/v1/auth/auth.module';
-import { UserEntity } from './api/v1/users/entities/user.entity';
 import { PostsModule } from './api/v1/posts/posts.module';
-import { PostEntity } from './api/v1/posts/entities/post.entity';
 import { CommentsModule } from './api/v1/comments/comments.module';
-import { CommentEntity } from './api/v1/comments/entities/comment.entity';
 import { SharedModule } from './shared/shared.module';
 import { LikesModule } from './api/v1/likes/likes.module';
-import { LikeEntity } from './api/v1/likes/entities/like.entity';
 import { PostExistenceMiddleware } from './middlewares/post-existence.middleware';
 import { UserExistenceMiddleware } from './middlewares/user-existence.middleware';
 import { ActivityCatcherMiddleware } from './middlewares/activity-catcher.middleware';
+import { dataSourceOptions } from 'db/data-source';
 
 @Module({
   imports: [
@@ -25,19 +22,7 @@ import { ActivityCatcherMiddleware } from './middlewares/activity-catcher.middle
       envFilePath: '.env',
       isGlobal: true
     }),
-    TypeOrmModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.getOrThrow('MYSQL_DB_HOST'),
-        port: configService.getOrThrow('MYSQL_DB_PORT'),
-        username: configService.getOrThrow('MYSQL_DB_USERNAME'),
-        password: configService.getOrThrow('MYSQL_DB_PASSWORD'),
-        database: configService.getOrThrow('MYSQL_DB'),
-        entities: [UserEntity, PostEntity, CommentEntity, LikeEntity],
-        synchronize: configService.getOrThrow('SYNCRONIZE_DB') === 'true'
-      }),
-      inject: [ConfigService]
-    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
     JwtModule.register({
       global: true,
       secret: process.env.TOKEN_SECRET,
